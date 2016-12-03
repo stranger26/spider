799e124a837bff47ba3e5b8bcdd21db3867656e2
Ho Chin Chee <alexho97@hotmail.com>
2016-12-03 16:23:39 +0800

    Follow URL

--- spider/spiders/main.py
+++ spider/spiders/main.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
+from scrapy.http import Request
 
 
 class MySpider(CrawlSpider):
@@ -14,3 +15,8 @@ class MySpider(CrawlSpider):
                 'link': title.xpath("a/@href").extract_first(),
                 'title': title.xpath("a/text()").extract_first()
                 }
+        next_page = response.xpath('//a[@class="button next"]/@href').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield Request(next_page, callback=self.parse)
+   
